895c935ab0d2a2f6a38f168e0eed35b34720df52
rehamAlii29 <99566872+rehamAlii29@users.noreply.github.com>
2022-03-22 19:25:33 +0200

    commit on main

--- lib/LoginUser.dart
+++ lib/LoginUser.dart
@@ -11,24 +11,40 @@ class _LoginUserState extends State<LoginUser> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * .3,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Image.asset('assets/images/logo.png')]),
-        ),
-        const Text("proceed with your"),
-        const SizedBox(height: 5),
-        const Text(
-          "login",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
+        body: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .3,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [Image.asset('assets/images/logo.png')]),
+          ),
+          const Text("proceed with your"),
+          const SizedBox(height: 5),
+          const Text(
+            "Login",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Form(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('username / email'),
+              TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Duran0124r", suffixIcon: Icon(Icons.person))),
+            ],
+          ))
+        ],
+      ),
     ));
   }
 }
